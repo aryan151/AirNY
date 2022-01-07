5f3ff30c0df3eae103dcf3e881c53fb33b89726c
aryan <aryanattula@gmail.com>
2022-01-06 20:21:06 -0500

    splash part 1

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,9 +1,36 @@
+import './Home.css'
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux"  
+import { useHistory } from 'react-router-dom'; 
+import {fetchListings} from '../../store/listings' 
+
 function Home() {
- 
-  return (
-    <div className='layout__splash_container'> 
-        <p>test</p>
+    const history = useHistory()
+    const dispatch = useDispatch()
+
+    const splashbtnclick = (e) => {
+      e.preventDefault();
+      history.push("/listings")
+    }
+  
+    useEffect(()=> {
+        dispatch(fetchListings()) 
+    }, [dispatch])
+
+  return (   
+    <div>
+        <div className="Homepagefirst"> 
+            <form className="topsplash" onSubmit={splashbtnclick}>
+            <div>Not sure where to go? Perfect.</div>
+            <button className="topbtnsplash" type="submit">I'm flexible</button>
+        </form>
     </div>
+    <div>
+    <p>test</p> 
+    </div>
+  </div>
+
+    
 )
 };
-export default Home;    
+export default Home;      
